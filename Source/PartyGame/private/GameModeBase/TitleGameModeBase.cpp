dcdfc07f0aa89789e03271e7c5abe2b2a2a6f46f
oHixDio <hry66H@outlook.jp>
2024-03-17 22:47:58 +0900

    Create PartyGameInstance

--- Source/PartyGame/private/GameModeBase/TitleGameModeBase.cpp
+++ Source/PartyGame/private/GameModeBase/TitleGameModeBase.cpp
@@ -40,7 +40,4 @@ void ATitleGameModeBase::BeginPlay()
 		}
 	}
 	
-
-	
-	
 }
---
+++ Source/PartyGame/public/PartyGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PartyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARTYGAME_API UPartyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
